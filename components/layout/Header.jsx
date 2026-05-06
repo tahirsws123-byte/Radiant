9ad1eb42a05a4d41a9f82633c3c1d4fc4a098678
coderzzVeerendra <mrvirendra.dewas@gmail.com>
2026-05-06 13:53:59 +0530

    header correction

--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -242,7 +242,7 @@ function Header() {
                           <div className="-my-6 divide-y divide-white/10">
                             <div className="space-y-2 py-6">
                               <NavLink
-                                href="#"
+                                to="/blog"
                                 className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
                               >
                                 Blog
