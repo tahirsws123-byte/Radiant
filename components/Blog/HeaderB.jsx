b53ad4bbd4eabe07fdf44c80e992b2a380864556
coderzzVeerendra <mrvirendra.dewas@gmail.com>
2026-05-06 13:45:19 +0530

    header changes

--- components/Blog/HeaderB.jsx
+++ components/Blog/HeaderB.jsx
@@ -218,8 +218,8 @@ export default function HeaderB() {
                         />
                       </svg>
                     </button>
-                    <div>
-                      <div
+                    <el-dialog>
+                      <dialog
                         id="mobile-menu"
                         className="backdrop:bg-transparent lg:hidden"
                       >
@@ -227,7 +227,7 @@ export default function HeaderB() {
                           tabIndex="0"
                           className="fixed inset-0 focus:outline-none"
                         >
-                          <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
+                          <el-dialog-panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
                             <div className="flex items-center justify-between">
                               <a href="#" className="-m-1.5 p-1.5">
                                 <span className="sr-only">Your Company</span>
@@ -281,10 +281,10 @@ export default function HeaderB() {
                                 </div>
                               </div>
                             </div>
-                          </div>
+                          </el-dialog-panel>
                         </div>
-                      </div>
-                    </div>
+                      </dialog>
+                    </el-dialog>
                   </div>
                 </nav>
               </div>
--- components/layout/Header.jsx
+++ components/layout/Header.jsx
@@ -195,8 +195,8 @@ function Header() {
                     />
                   </svg>
                 </button>
-                <div>
-                  <div
+                <el-dialog>
+                  <dialog
                     id="mobile-menu"
                     className="backdrop:bg-transparent lg:hidden"
                   >
@@ -204,7 +204,7 @@ function Header() {
                       tabIndex="0"
                       className="fixed inset-0 focus:outline-none"
                     >
-                      <div className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
+                      <el-dialog-panel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-gray-900 p-6 sm:max-w-sm sm:ring-1 sm:ring-gray-100/10">
                         <div className="flex items-center justify-between">
                           <a href="#" className="-m-1.5 p-1.5">
                             <span className="sr-only">Your Company</span>
@@ -242,7 +242,7 @@ function Header() {
                           <div className="-my-6 divide-y divide-white/10">
                             <div className="space-y-2 py-6">
                               <NavLink
-                                to="/blog"
+                                href="#"
                                 className="-mx-3 block rounded-lg px-3 py-2 text-base/7 font-semibold text-white hover:bg-white/5"
                               >
                                 Blog
@@ -258,10 +258,10 @@ function Header() {
                             </div>
                           </div>
                         </div>
-                      </div>
+                      </el-dialog-panel>
                     </div>
-                  </div>
-                </div>
+                  </dialog>
+                </el-dialog>
               </div>
             </nav>
           </div>
