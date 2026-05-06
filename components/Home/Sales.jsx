869a17e0abe157590ccda731bc5cc0d55ba0fc28
coderzzVeerendra <mrvirendra.dewas@gmail.com>
2026-05-06 12:31:30 +0530

    fixed bg-img path

--- components/Home/Sales.jsx
+++ components/Home/Sales.jsx
@@ -23,7 +23,7 @@ function Sales() {
         <div className="mt-10 grid grid-cols-1 gap-4 sm:mt-16 lg:grid-cols-6 lg:grid-rows-2">
           <div className="max-lg:rounded-t-4xl lg:col-span-3 lg:rounded-tl-4xl relative flex flex-col overflow-hidden rounded-lg bg-white shadow-xs ring-1 ring-black/5">
             <div className="relative h-80 shrink-0">
-              <div className="h-80 bg-[url(../../src/assets/s1.png)] bg-size-[1000px_560px] bg-position-[left_-109px_top_-112px] bg-no-repeat"></div>
+              <div style={{ backgroundImage: `url(${img1})` }} className="h-80 bg-size-[1000px_560px] bg-position-[left_-109px_top_-112px] bg-no-repeat"></div>
               <div className="absolute inset-0 z-10 bg-linear-to-t from-white to-50%"></div>
             </div>
             <div className="relative p-10">
